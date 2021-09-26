02b7e7bef4859f746a53d8c97f7622436d8105d5
souadelmansouri <souadelmansouri88@gmail.com>
2021-09-26 09:40:46 +0100

    added login link to header

--- src/header.js
+++ src/header.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import './home.css' 
 function Header() {
     return (
         <div>
             <div className="header_logo"> 
-                <Link to="/login" className="login_img">
+               
+                <Link to="/" className="login_img">
                     <img src="../bell.png" alt="" />
                 </Link>
                 
-                 <h5>My Alerts</h5>
+                <div> <h5>My Alerts</h5></div>
+                <div></div>
+                <Link to="/login" className="header_login">
+                     <AccountCircleIcon className="AccountCircleIcon" />
+                     <h2>login</h2>
+               </Link>
             </div>
            <div className="header_buttom"></div> 
         </div>
